e85a6dcb7ee2c067678bed3682edce1bf1f72f3b
serhii_koshelev <Serhii_Koshelev@epam.com>
2021-01-03 23:08:29 +0200

    Upload labs to git

---
+++ CNnet_Lab5/CNLab5/Models/SocketLogic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CNnet_Lab1.Models;
+
+namespace UDPSocket
+{
+    public class SocketLogic
+    {
+        
+        
+        public static int localPort; // port for incoming messages
+        public static int remotePort; // port for outcoming messages
+        public static Socket listeningSocket;
+        public static void ListenSocket()
+        {
+            try
+            {
+                //Прослушиваем по адресу
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), localPort);
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    // recieving message
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // amount of recieved bytes
+                    byte[] data = new byte[256]; // buffer
+
+                    // sender address
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    // output a message
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(),
+                                                    remoteFullIp.Port, builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                SocketLogic.CloseSocket();
+            }
+        }
+        // close socket
+        public static void CloseSocket()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+        }
+    }
+}
---
+++ CNnet_Lab5/CNLab5/Program.cs
@@ -0,0 +1,53 @@
+﻿using CNnet_Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using UDPSocket;
+
+namespace UDPSocket
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+    
+            Console.Write("Please enter port number for incoming messages: ");
+            SocketLogic.localPort = Int32.Parse(Console.ReadLine());
+            Console.Write("Please enter port number for outcoming messages: ");
+            SocketLogic.remotePort = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("To send a message type it and hit Enter");
+            Console.WriteLine();
+
+            try
+            {
+                SocketLogic.listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(SocketLogic.ListenSocket);
+                listeningTask.Start();
+
+                // Send messages to different ports
+                while (true)
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), SocketLogic.remotePort);
+                    SocketLogic.listeningSocket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                SocketLogic.CloseSocket();
+            }
+        }
+    }
+}
